e937729f65bece7a0fb45e7c513fff5b8cdfa71c
Liuming <lfan.canada@gmail.com>
2021-05-27 00:00:08 +0000

    add partial, update order details page, update orders_controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,8 @@ class OrdersController < ApplicationController
 
   def show
     @order = Order.find(params[:id])
+    # @line_items = LineItem.where("order_id = #{params[:id]}")
+    @line_items = @order.line_items
   end
 
   def create
