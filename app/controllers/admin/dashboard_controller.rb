d0411e1b61b0b72f54fe921a5deb6ac4643af7ae
Liuming <lfan.canada@gmail.com>
2021-05-27 04:42:37 +0000

    add admin authentication/security

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,10 @@
 class Admin::DashboardController < ApplicationController
+  http_basic_authenticate_with = {
+    :username => ENV['ADMIN_USER'], 
+    :password => ENV['ADMIN_PASSWORD']
+  }
+  
   def show
   end
+
 end
