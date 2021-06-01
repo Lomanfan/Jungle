0be0cd384bc726ad92e3c685d985a327cc4be66b
Liuming <lfan.canada@gmail.com>
2021-06-01 03:41:40 +0000

    generate user_spec file

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+
+  describe 'Validations' do
+    # validation examples here
+  end
+
+  describe '.authenticate_with_credentials' do
+    # examples for this class method here
+  end
+
+end
