fc38e1342bd5648900fc7744fe6d92b10ba40020
Liuming <lfan.canada@gmail.com>
2021-06-02 02:12:30 +0000

    add category and products/Rspec before :each

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -12,7 +12,7 @@ require 'rspec/rails'
 require "capybara/rails"
 require "capybara/rspec"
 require "capybara/poltergeist"
-Capybara.javascript_driver = .poltergeist
+Capybara.javascript_driver = :poltergeist
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
