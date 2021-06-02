d403387c589e95325d90725fa13ae53e1d84c4c9
Liuming <lfan.canada@gmail.com>
2021-06-02 08:24:12 +0000

    testing, feature spec: product details

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -17,12 +17,23 @@ RSpec.feature "ProductDetails", type: :feature, js: true do  #TODO
     end
   end
   
-  scenario "When product image is clicked, show product details" do
+  scenario "When product image or name is clicked, show product details" do
     visit root_path
-    first('.product > header').click
+    first('.product > header').click     #ambiguity resolution #TODO
     save_screenshot 'product_details.png'
     # puts page.html
-    expect(page).to have_css 'section.products-show'
+    expect(page).to have_css 'section.products-show', count: 1
+  end
+
+  scenario "When product Details button is clicked, show product details" do
+    visit root_path
+    find_link("Details »", href: product_path(Product.first.id)).click
+    save_screenshot 'product_details2.png'
+    expect(page).to have_text(Product.first.name).and have_css "article.product-detail", count: 1
   end
 
 end
+
+
+#click_link("Details", match: :first)
+#first('article.product').click
