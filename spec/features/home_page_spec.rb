3fd5d5b530ee6965f44161159d60e9fa4620ab55
Liuming <lfan.canada@gmail.com>
2021-06-02 04:34:21 +0000

    fix broken image

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -10,6 +10,7 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
       @category.products.create!(
         name:  Faker::Hipster.sentence(3),
         description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
         quantity: 10,
         price: 64.99
       )
@@ -22,8 +23,9 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
 
     # DEBUG / VERIFY
     save_screenshot
-    expect(page).to have_css 'article.product'
+    
+    expect(page).to have_css 'article.product', count: 10
+
   end
   
-
 end
