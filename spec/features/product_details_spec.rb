c54c0c234ffe71f9783419086107a1db985ca929
Liuming <lfan.canada@gmail.com>
2021-06-02 07:04:24 +0000

    testing, feature spec: product details

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.feature "ProductDetails", type: :feature, js: true do  #TODO
+  
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+  
+  scenario "When product image is clicked, show product details" do
+    visit root_path
+
+  save_screenshot
+  end
+end
